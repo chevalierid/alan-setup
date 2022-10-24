aafd056a531489c4034dc83a513a032a6f93f92b
chevalierid <daphne.chevalier@ubc.ca>
2022-10-24 11:14:38 -0700

    updates to sd tester

--- sd_test_sensor/sd_test_sensor.ino
+++ sd_test_sensor/sd_test_sensor.ino
@@ -4,16 +4,19 @@
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include "Adafruit_TSL2591.h"
+#include <Adafruit_AS7341.h>
 
 File myFile;
+File root;
 RTClib myRTC;
 int j;
 String print_data;
 String print_lux;
 
 Adafruit_TSL2591 tsl = Adafruit_TSL2591(2591); // pass in a number for the sensor identifier (for your use later)
+Adafruit_AS7341 as7341;
 
-void configureSensor(void)
+void configureTSL(void)
 {
   // You can change the gain on the fly, to adapt to brighter/dimmer light situations
   //tsl.setGain(TSL2591_GAIN_LOW);    // 1x gain (bright light)
@@ -24,7 +27,7 @@ void configureSensor(void)
   // longer timelines are slower, but are good in very low light situtations!
   //tsl.setTiming(TSL2591_INTEGRATIONTIME_100MS);  // shortest integration time (bright light)
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_200MS);
-  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS); 
+  tsl.setTiming(TSL2591_INTEGRATIONTIME_300MS);
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_400MS);
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_500MS);
   // tsl.setTiming(TSL2591_INTEGRATIONTIME_600MS);  // longest integration time (dim light)
@@ -40,7 +43,7 @@ void configureSensor(void)
   Serial.println(F(""));
 }
 
-void advancedRead(void)
+uint16_t tslAdvancedRead(void)
 {
   // More advanced data read example. Read 32 bits with top 16 bits IR, bottom 16 bits full spectrum
   // That way you can do whatever math and comparisons you want!
@@ -48,21 +51,46 @@ void advancedRead(void)
   uint16_t ir, full;
   ir = lum >> 16;
   full = lum & 0xFFFF;
-  DateTime now = myRTC.now();
-  uint32_t timestamp = now.unixtime();
-  myFile = SD.open("readings.txt", FILE_WRITE);
 
-  if (myFile) {
-    Serial.println("Printing to file");
-    myFile.print(timestamp); 
-    myFile.print(F(","));
-    myFile.println(tsl.calculateLux(full, ir), 6);
-    myFile.close();
-    Serial.println("Done.");
-  } else {
-    Serial.println("error opening test.txt");
+  return tsl.calculateLux(full, ir);
+}
+
+String asRead() {
+  uint16_t as7341_readings[12];
+  String allReadings;
+  if (!as7341.readAllChannels(as7341_readings)){
+    Serial.println("Error reading all channels!");
+    return;
   }
- 
+  for (int i = 0; i < 12; i++) {
+    allReadings = allReadings + "," + String(as7341_readings[i]);
+    Serial.println(allReadings);
+  }
+  return allReadings;
+}
+
+void printDirectory(File dir, int numTabs) {
+   while (true) {
+
+      File entry = dir.openNextFile();
+      if (! entry) {
+         // no more files
+         break;
+      }
+      for (uint8_t i = 0; i < numTabs; i++) {
+         Serial.print('\t');
+      }
+      Serial.print(entry.name());
+      if (entry.isDirectory()) {
+         Serial.println("/");
+         printDirectory(entry, numTabs + 1);
+      } else {
+         // files have sizes, directories do not
+         Serial.print("\t\t");
+         Serial.println(entry.size(), DEC);
+      }
+      entry.close();
+   }
 }
 
 void setup() {
@@ -71,12 +99,24 @@ void setup() {
   while (!Serial) {
     ; // wait for serial port to connect
   }
+  Serial.print("Initializing SD card...");
   if (!SD.begin(53)) {
     Serial.println("Initialization failed!");
     while(1);
   }
-  Serial.println("Initialization done");
+  Serial.println("Initialization done.");
+  // open file
+  myFile = SD.open("not_test.txt", FILE_WRITE);
+  if (myFile) {
+    Serial.print("Writing to file...");
+    myFile.println("Timestamp,Intensity,AS0,AS1,AS2,AS3,AS4,AS5,AS6,AS7,AS8,AS9,AS10,AS11");
+    myFile.close();
+    Serial.println("Done.");
+  } else {
+    Serial.println("couldn't open in setup");
+  }
 
+  
   if (tsl.begin()) 
   {
     Serial.println(F("Found a TSL2591 sensor"));
@@ -86,30 +126,57 @@ void setup() {
     Serial.println(F("No sensor found ... check your wiring?"));
     while (1);
   }
-  
-  if (SD.exists("readings.txt")) {
-    SD.remove("readings.txt");
-  } 
-  // FIXME this shouldn't be in final version
+  configureTSL();
 
-  myFile = SD.open("readings.txt",FILE_WRITE);
-  if (myFile) {
-    myFile.println("Timestamp,Intensity");
-    myFile.close();
+  if (!as7341.begin()){
+    Serial.println("Could not find AS7341");
+    while (1) { delay(10); }
   }
   
-  configureSensor();
+  as7341.setATIME(100);
+  as7341.setASTEP(999);
+  as7341.setGain(AS7341_GAIN_256X);
+
+  Serial.println("before");  
+  root = SD.open("/");
+  printDirectory(root, 0);
+  
+//  if (SD.exists("READINGS.TXT")) {
+//    Serial.println("it's here");
+//    SD.remove("readings.txt");
+//  } 
+  // FIXME this shouldn't be in final version
+  Serial.println("after");  
+  root = SD.open("/");
+  printDirectory(root, 0);
+  
+  
+  // FIXME move this into loop and have it change name every day
   j = 0;
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (j < 10) {
-    advancedRead();
-  } else {
-    Serial.println("done");
+  myRTC.setAlarm1(rtc.now() + TimeSpan(0, 0, 0, 5), DS3231_A1_Minute) // In 5 seconds time from now
+  // FIXME this should be 5min not 5sec
+  if (alarmFired()) {
+    DateTime now = myRTC.now();
+    uint32_t timestamp = now.unixtime();
+    String as;
+    myFile = SD.open("not_test.txt", FILE_WRITE);
+    if (myFile) {
+      Serial.println("Printing to file");
+      myFile.print(timestamp);
+      myFile.print(",");
+      myFile.print(tslAdvancedRead(), 6);
+      as = asRead();
+      myFile.print(as);
+      //Serial.print(as);
+      myFile.write("\n");
+      myFile.close();
+      Serial.println("Done.");
+    } else {
+      Serial.println("couldn't open in loop");
+    }
   }
-  j++;
-  //delay(2000);
-  
 }
