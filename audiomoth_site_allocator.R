0de74b14a4f7fde4e91c846e18809328aa6f4456
chevalierid <daphne.chevalier@ubc.ca>
2023-06-07 14:26:14 -0700

    functioning audiomoth site allocator giving 1 2 3 8 9 12

--- audiomoth_site_allocator.R
+++ audiomoth_site_allocator.R
@@ -1,35 +1,45 @@
 rm(list = ls(all = TRUE))
-locations <- read.csv(file="~/Documents/alan_setup/sites_2023.csv",header=T,sep=",")
+locations <- read.csv(file="~/Documents/alan_setup/2023_ALAN_streetlight_sites.csv",header=T,sep=",")
 locations <- data.frame(cbind(locations$Latitude,locations$Longitude))
 colnames(locations) <- list("latitude","longitude")
+plot(locations$latitude,locations$longitude)
+text(locations$latitude+0.0001,locations$longitude, labels=c(1:12))
 
-#xy <- cbind(rnorm(1000),rnorm(1000))
+
+
+pointdist <- function(a, b) distm(c(locations[a,2],locations[a,1]), c(locations[b,2],locations[b,1]), fun = distHaversine)
 
 n <- 6
-best_avg <- 0
+best_avg <- 0.00374
 best_set <- NA
-for (i in 1:12){
-  subset <- locations[sample(1:nrow(locations),n),]
-  avg <- mean(dist(subset))
-  if (avg > best_avg & !any(dist(subset)<0.001)) {
-    best_avg <- avg
-    best_set <- subset
+dist_pass <- FALSE
+all_sub_sets <- combn(1:12, 6, simplify = FALSE)
+counter <- 1
+# while the sub_set's distance matrix has any value that's less than 0.001
+while (!dist_pass & counter < 925){
+  for (i in 1:12){
+    #sub_set <- locations[sample(1:nrow(locations),n),]
+    print(all_sub_sets[counter],)
+    print(locations[unlist(all_sub_sets[counter]),])
+    sub_set <- locations[unlist(all_sub_sets[counter]),]
+    print("sub_set: ")
+    print(sub_set)
+    avg <- mean(dist(sub_set))
+    if (avg > best_avg) {
+      dist_pass = !any(dist(sub_set)<0.0013)
+      print("dist_pass: ")
+      print(dist_pass)
+      if (dist_pass) {
+        print("passed the test: ")
+        print(dist(sub_set)) 
+      }
+      best_avg <- avg
+      best_set <- sub_set
+    }
+    counter <- counter + 1
   }
 }
 
-# library(geosphere)
- distm(c(locations[7,2],locations[7,1]), c(locations[3,2],locations[3,1]), fun = distHaversine)
-# 
-# checkdist <- function(newpt,bestset) { 
-#   for (i in nrow(bestset)) {
-#     if (distm(c(newpt[2],newpt[1]), c(bestset[i,2],bestset[i,1]), fun = distHaversine)<100) {
-#       return(0)
-#     }
-#   }
-#   return(1)
-# }
-
-
-plot(locations$latitude,locations$longitude)
+plot(locations$latitude,locations$longitude, xlim = rev(range(locations$latitude)), ylim = rev(range(locations$longitude)))
 text(locations$latitude+0.0001,locations$longitude, labels=c(1:12))
 points(best_set$latitude,best_set$longitude,col="red",pch=16)
